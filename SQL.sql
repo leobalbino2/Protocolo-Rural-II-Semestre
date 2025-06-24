e465ae12c12b694d0be3fc1e8660afa7b7f0bffa
Leonardo Balbino <leonardo.balbino2@fatec.sp.gov.br>
2025-06-23 22:38:48 -0300

    Correção no SQL

--- SQL.sql
+++ SQL.sql
@@ -79,6 +79,15 @@ INSERT INTO parametros (indicador_id, descricao, valor) VALUES
 
 -- Parâmetros para indicador 3
 INSERT INTO parametros (indicador_id, descricao, valor) VALUES
+(3, 'Totalidade da RL com vegetação remanescente/restaurada/em restauração na propriedade (conforme o Capítulo IV da Lei Federal nº. 12.651/2012).', 5),
+(3, 'Pelo menos 50% da RL com vegetação remanescente/restaurada/em restauração na propriedade (conforme o Capítulo IV da Lei Federal nº. 12651/2012) e o restante compensada na região/bacia hidrográfica (conforme o artigo 66 da Lei Federal nº. 12.651/2012).', 4),
+(3, 'Pelo menos 50% da RL com vegetação remanescente/restaurada/em restauração na propriedade (conforme o Capítulo IV da Lei Federal nº. 12651/2012) e restante compensada fora de região/bacia hidrográfica (conforme o artigo 66 da Lei Federal nº. 12.651/2012).', 3),
+(3, 'Menos de 50% da RL com vegetação remanescente/restaurada/em restauração na propriedade (conforme o Capítulo IV da Lei Federal nº. 12651/2012), e o restante compensada na região/bacia hidrográfica ou 100% da RL compensada na região/bacia hidrográfica (conforme o artigo 66 da Lei Federal nº. 12.651/2012).', 2),
+(3, 'Menos de 50% da RL com vegetação remanescente/restaurada/em restauração na propriedade (conforme o Capítulo IV da Lei Federal nº. 12651/2012), e o restante compensada fora da região/bacia hidrográfica 100% da RL compensada fora da região/bacia hidrográfica (conforme o artigo 66 da Lei Federal nº. 12.651/2012).', 1),
+(3, 'Sem RL e/ou compensação.', 0);
+
+
+-- Parâmetros para indicador 4
 INSERT INTO parametros (indicador_id, descricao, valor) VALUES
 (4, 'A propriedade está cadastrada no CAR, possui PRA (ou dispensa) e tem suas licenças ambientais e outorgas (ou dispensa de outorga) atualizadas em conformidade com as atividades que exerce.', 5),
 (4, 'A propriedade está cadastrada no CAR, não possui PRA, e tem suas licenças ambientais e outorgas (ou dispensa de outorga) atualizadas em conformidade com as atividades que exerce.', 4),
@@ -86,5 +95,3 @@ INSERT INTO parametros (indicador_id, descricao, valor) VALUES
 (4, 'A propriedade não está cadastrada no CAR, e suas licenças ambientais e outorgas não estão atualizadas em conformidade com as atividades que exerce. ', 2),
 (4, 'A propriedade não está cadastrada no CAR e não possui parte das licenças ambientais e outorgas em conformidade com as atividades que exerce.', 1),
 (4, 'A propriedade não está cadastrada no CAR e não tem as licenças ambientais e outorgas que permitem exercer suas atividades.', 0);
-
--- Parâmetros para indicador 4
